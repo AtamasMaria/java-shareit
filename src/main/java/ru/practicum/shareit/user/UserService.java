f6c597e2c0ea6d3a9659b0db45bf260d2966d800
Атамас Мария <atamasmari@yandex.ru>
2023-04-02 00:45:36 +0300

    ТЗ 13.2

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -1,3 +1,4 @@
+
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
@@ -19,14 +20,17 @@ public class UserService {
         validateEmail(user);
         return UserMapper.toUserDto(userStorage.create(user));
     }
+
     public Collection<UserDto> getAll() {
         return userStorage.getAll().stream()
                 .map(UserMapper::toUserDto)
                 .collect(Collectors.toList());
     }
+
     public UserDto getById(int id) {
         return UserMapper.toUserDto(userStorage.get(id));
     }
+
     public UserDto updateById(int id, UserDto userDto) {
         User newUser = UserMapper.toUser(userDto);
         checkDuplicateEmail(newUser);
@@ -37,21 +41,25 @@ public class UserService {
             user.setName(newUser.getName());
         return UserMapper.toUserDto(userStorage.update(user));
     }
+
     public UserDto delete(int id) {
         return UserMapper.toUserDto(userStorage.delete(id));
     }
+
     void validateEmail(User user) {
         checkDuplicateEmail(user);
         if (user.getEmail() == null || user.getEmail().isBlank() || (!user.getEmail().contains("@"))) {
             throw new ValidateException("Неверно заполнено полу эл.почта");
         }
     }
+
     void checkDuplicateEmail(User user) {
         if (userStorage.getAll().stream().anyMatch(u -> u.getEmail().equals(user.getEmail())
                 && u.getId() != user.getId())) {
             throw new DuplicateEmailException("Пользователь с таким Email уже существует");
         }
     }
+
     public void checkUserId(int id) {
         if (getAll().stream().noneMatch(user -> user.getId() == id))
             throw new NotFoundException("Пользователь с таким ID не найден");
