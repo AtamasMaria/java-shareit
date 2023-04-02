daceca7f7af26b02db97c832b15ab945ab8f22a6
Атамас Мария <atamasmari@yandex.ru>
2023-04-02 03:15:26 +0300

    ТЗ 13

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,44 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * TODO Sprint add-controllers.
- */
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public UserDto createUser(@Valid @RequestBody UserDto userDto) {
+        log.debug("POST-запрос на создание нового пользователя.");
+        return userService.create(userDto);
+    }
+    @GetMapping("/{userId}")
+    public UserDto getUserById(@PathVariable Long userId) {
+        log.debug("GET-запрос на вывод пользователя по идентификатору.");
+        return userService.getUserById(userId);
+    }
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        log.debug("GET-запрос на вывод всех пользователей.");
+        return userService.getAll();
+    }
+    @PatchMapping("/{userId}")
+    public UserDto updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
+        log.debug("PATCH-запрос на обновление пользователя.");
+        return userService.update(userId, userDto);
+    }
+    @DeleteMapping("/{userId}")
+    public void deleteUserById(@PathVariable Long userId) {
+        log.debug("DELETE-запрос на удаление пользователя.");
+        userService.deleteUserById(userId);
+    }
 }
+
